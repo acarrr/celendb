6ba6e5f73faf14d6a659e0782152a8b06dd9b30b
acar <64407700+acarrr@users.noreply.github.com>
2020-05-05 14:01:23 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,228 @@
+const fs = require("fs");
+
+function parse(str) {
+  return Function('"use strict";return (' + str + ')')();
+}
+
+class Database {
+  /**
+   * @param {String} filePath
+  */
+  constructor(filePath) {
+    this.filePath = filePath;
+
+    if(!fs.existsSync(this.filePath)) {
+      fs.writeFileSync(this.filePath, "{}");
+    }
+  }
+
+  /**
+   * @param {String} key
+   * @param {any} value
+   * @returns {any}
+  */
+
+  set(key, value) {
+    let obj = JSON.parse(fs.readFileSync(this.filePath, "utf8"));
+
+    if(key.includes(".")) {
+      let data = new String();
+
+      for(let i = 0; i < key.split(".").length; i++) {
+        if(i == 0) {
+          data += ("obj");
+        }
+
+        data += (`["${key.split(".")[i]}"]`);
+
+        if(typeof eval(data) == "undefined") {
+          eval(`${data} = {}`);
+        }
+      }
+
+      data += (" = ");
+      data += (`parse('${JSON.stringify(value)}')`);
+
+      eval(data);
+
+      fs.writeFileSync(this.filePath, JSON.stringify(obj));
+    } else {
+      obj[key] = value;
+
+      fs.writeFileSync(this.filePath, JSON.stringify(obj));
+    }
+
+    return this.get(key);
+  }
+
+  /**
+   * @param {String} key
+   * @return {any}
+  */
+  get(key) {
+    let obj = JSON.parse(fs.readFileSync(this.filePath, "utf8"));
+
+    if(key.includes(".")) {
+      let data = new String();
+
+      for(let i = 0; i < key.split(".").length; i++) {
+        if(i == 0) {
+          data += ("obj");
+        }
+
+        data += (`["${key.split(".")[i]}"]`);
+
+        if(typeof eval(data) == "undefined") {
+          return undefined;
+          break;
+        }
+      }
+
+      return eval(data);
+    } else {
+      return JSON.parse(fs.readFileSync(this.filePath, "utf8"))[key];
+    }
+  }
+
+  /**
+   * @param {String} key
+   * @return {any}
+  */
+  delete(key) {
+    let obj = JSON.parse(fs.readFileSync(this.filePath, "utf8"));
+
+    if(key.includes(".")) {
+      let data = new String();
+
+      for(let i = 0; i < key.split(".").length; i++) {
+        if(i == 0) {
+          data += ("obj");
+        }
+
+        if(i == (key.split(".").length - 1)) {
+          eval(`delete ${data}["${key.split(".")[i]}"]`);
+          fs.writeFileSync(this.filePath, JSON.stringify(obj));
+
+          return true;
+          break;
+        } else {
+          data += (`["${key.split(".")[i]}"]`);
+        }
+
+        if(typeof eval(data) == "undefined") {
+          return false;
+          break;
+        }
+      }
+    } else {
+      delete obj[key];
+      fs.writeFileSync(this.filePath, JSON.stringify(obj));
+
+      return true;
+    }
+  }
+
+  /**
+   * @param {String} has
+   * @return {Boolean}
+  */
+  has(key) {
+    return (typeof this.get(key) != "undefined");
+  }
+
+  /**
+   * @param {String} key
+   * @param {any} value
+   * @returns {any}
+  */
+  push(key, value) {
+    let arr = ((this.has(key) && Array.isArray(this.get(key))) ? this.get(key) : new Array());
+    arr.push(value);
+    return this.set(key, arr);
+  }
+
+  /**
+   * @param {String} key
+   * @param {Function} value
+   * @return {any}
+  */
+  update(key, f) {
+    let data = (this.has(key) ? this.get(key) : 0);
+    this.set(key, f(data));
+  }
+
+  /**
+   * @param {String} key
+   * @param {Number} index
+   * @param {any} value
+  */
+  updateEl(key, index, value) {
+    let data = (this.has(key) ? this.get(key) : false);
+    let newData = new Array();
+
+    if(!Array.isArray(data)) {
+      return false;
+    }
+
+    for(let i = 0; i < data.length; i++) {
+      if(i == index) {
+        newData.push(value);
+      } else {
+        newData.push(data[i]);
+      }
+    }
+
+    this.set(key, newData);
+
+    return newData;
+  }
+
+  /**
+   * @param {String} key
+   * @param {Number} index
+   * @param {any} value
+  */
+  delElByIndex(key, index) {
+    let data = (this.has(key) ? this.get(key) : false);
+    let newData = new Array();
+
+    if(!Array.isArray(data)) {
+      return false;
+    }
+
+    for(let i = 0; i < data.length; i++) {
+      if(i != index) {
+        newData.push(data[i]);
+      }
+    }
+
+    this.set(key, newData);
+
+    return newData;
+  }
+
+  /**
+   * @param {String} key
+   * @param {any} filter
+  */
+  delElByVal(key, filter) {
+    let data = (this.has(key) ? this.get(key) : false);
+    let newData = new Array();
+
+    if(!Array.isArray(data)) {
+      return false;
+    }
+
+    for(let i = 0; i < data.length; i++) {
+      if(data[i] != filter) {
+        newData.push(data[i]);
+      }
+    }
+
+    this.set(key, newData);
+
+    return newData;
+  }
+}
+
+module.exports = Database;
